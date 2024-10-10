e2497ed90088cdfa1b012c3cecf45be299d11981
unknown <yourstruly.bill@gmail.com>
2024-10-10 22:03:27 +0400

    add dynamic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
-function App() {
+
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const  Header = () => {
+  const index  = genRandomInt(2);
+
+  const name = reactDescriptions[index];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {`${name} React concepts you will need for almost any app you are
+        going to build!`}
+      </p>
+    </header>
+);
+};
+
+const CoreConcept = (props) => {
+  const { title, description, image } = props;
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+};
+
+export const App = () => {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials1</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
